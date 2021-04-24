cfbf8cacd54f2bc9c5174d7e29524f64c4699493
dilya1983 <dkhamidova1@gmail.com>
2021-04-24 16:52:58 -0400

    Third commit

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -2,7 +2,9 @@ package day1;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("HelloWorld");
         System.out.println("Adding GitHub Now");
+        System.out.println("Yay Stuffs are on GitHub");
     }
 }
